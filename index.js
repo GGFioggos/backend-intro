3be3603822960f4d11e4580d2251f2cc5aa4ff44
moschidisg <mosxidismosxidis@gmail.com>
2022-10-24 19:04:53 +0300

    refactor using express

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const http = require('http');
-const { readFile } = require('fs/promises');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const port = 8000;
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.get('/contact-me', function (req, res) {
+    res.sendFile(path.join(__dirname, '/contact-me.html'));
+});
+
+app.get('*', function (req, res) {
+    res.statusCode = 404;
+    res.sendFile(path.join(__dirname, '/404.html'));
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`);
+});
+
+/*
 const handler = async (req, res) => {
     if (req.url === '/' || req.url === '/index.html') {
         const data = await readFile('./index.html', 'utf8');
@@ -26,10 +49,7 @@ const handler = async (req, res) => {
         const data = await readFile('./404.html', 'utf8');
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
-        res.end();
+        res.end(data);
     }
 };
-
-const server = http.createServer(handler);
-
-server.listen(port);
+*/
